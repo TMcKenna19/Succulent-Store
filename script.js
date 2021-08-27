ebb8553b75c54ae327ee869ce06c30aa49dabb7b
Tim <63888645+TMcKenna19@users.noreply.github.com>
2021-08-27 12:48:28 -0500

    succulentHome

---
+++ script.js
@@ -0,0 +1,7 @@
+function setNewImage(){
+  document.getElementById("img1").src ="images/assets/succulents-2.jpg"; 
+}
+
+function setOldImage(){
+    document.getElementById("img1").src ="images/assets/succulents-1.jpg";
+}
